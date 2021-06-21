2850e847e2dd4ca76b388ffd0dea1511e545afc4
Vineet <vineet98love@gmail.com>
2021-06-21 16:19:49 +0530

    block-BJaaep

---
+++ block-BJaaep/code/script.js
@@ -0,0 +1,15 @@
+let ul = document.querySelector("boxes");
+
+function handleClick() {
+
+}
+
+let li = document.createElement('box');
+li.classList.add('box');
+li.backgroundColor = 'red';
+
+li.addEventListener("click", function(event) {
+  
+})
+
+ul.append(li);
