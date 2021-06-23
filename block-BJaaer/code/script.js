d6db34e48c4d38c6dbaee11a59897d59397a3fcb
Vineet <vineet98love@gmail.com>
2021-06-23 22:15:18 +0530

    modification

--- block-BJaaer/code/script.js
+++ block-BJaaer/code/script.js
@@ -1,22 +1,56 @@
 let form = document.querySelector('form');
+let modal = document.querySelector(".modal_overlay");
+
+let modalInfo = document.querySelector(".relative");
 
 let userInfo =  {};
 
-function handleSubmit(event) {
+form.addEventListener("submit", (event) => {
     event.preventDefault();
+    let elements = event.target.elements;
 
-    userInfo.text = form.elements.name.value;
-    userInfo.email = form.elements.name.value;
+    userInfo.name = elements.name.value;
+    userInfo.email = elements.email.value;
 
-    userInfo.gender = form.elements.gender.value;
-    userInfo.color = form.elements.color.value;
+    userInfo.choice = elements.choice.value;
+    userInfo.color = elements.color.value;
 
-    userInfo.range = form.elements.range.value;
+    userInfo.range = elements.range.value;
     
-    userInfo.radio = form.elements.drone.value;
-    userInfo.terms = form.elements.terms.checked;
-   
-    console.log(userInfo);
+    userInfo.book = elements.book.value;
+    userInfo.terms =elements.terms.checked;
+
+    // console.log(userInfo);
+
+    modal.classList.remove("close"); 
+
+    
+    let close = document.querySelector(".modal_close");
+    close.addEventListener("click", () => {
+        modal.classList.remove("open");
+        modal.classList.add("close");
+    });
+    
+    displayInfo(userInfo);
+});
+
+function displayInfo( data = {} ) {
+    let h1 = document.createElement("h1");
+    h1.innerText = `Hello ${data.name}`;
+    let email = document.createElement("h1");
+    email.innerText = `Hello ${data.email}`;
+    let choice = document.createElement("h1");
+    choice.innerText = `Hello ${data.choice}`;
+    let color = document.createElement("h1");
+    color.innerText = `Hello ${data.color}`;
+    let range = document.createElement("h1");
+    range.innerText = `Hello ${data.range}`;
+    let book = document.createElement("h1");
+    book.innerText = `Hello ${data.book}`;
+    let terms = document.createElement("h1");
+    terms.innerText = `Hello ${data.terms}`;
+
+    modalInfo.append(h1,email, choice, color, range, book, terms);
 }
 
-form.addEventListener('submit', handleSubmit);
+
